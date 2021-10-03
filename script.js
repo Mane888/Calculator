954b007372fda715f7fbbe769796c44cf1d23b40
Branko <mane88@live.de>
2021-10-03 15:35:48 +0200

    commit 4

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ let operator = '';
 const sumBtn = document.querySelector('#sum');
 const numButtons = document.querySelectorAll('.numBtn');
 const operatorButtons = document.querySelectorAll('.operatorBtn');
-const screen = document.querySelector('.screen');
+const screen = document.querySelector('.upperNum');
+const screenSum = document.querySelector('.lowerNum');
+const clearBtn = document.querySelector('#c');
 
 
-numButtons.forEach((numBtn) => {
+//emptying both arrays and settion screen to default when pressing C
+clearBtn.addEventListener('click', () =>  {
+  a = [];
+  b = [];
+  screen.textContent = 0;
+  hideNull();
+});
+
+
+//hide lower row where the sum is displayed before there are 2 operators with an operand
+hideNull();
+function hideNull(){
+  if(screen.textContent === '0'){
+    document.querySelector('.lowerNum').classList.add('hideNull');
+  }
+  else{
+    document.querySelector('.lowerNum').classList.remove('hideNull');
+  };
+};
+
+
+numButtons.forEach((numBtn) =>  {
   numBtn.addEventListener('click', () =>{
     if(operator === ''){
       a.push(numBtn.textContent);
@@ -16,22 +39,30 @@ numButtons.forEach((numBtn) => {
     else if(operator != ''){
       b.push(numBtn.textContent);
       screen.textContent = `${a.join('')}${operator}${b.join('')}`
+      hideNull();
+      let sum = operate(a, b);
+      screenSum.textContent = sum;
+    };
+  });
+});
+
+operatorButtons.forEach((operatorBtn) =>  {
+  operatorBtn.addEventListener('click', ()  =>{
+    if(screen.textContent != '0'){
+    operator = operatorBtn.textContent;
+    screen.textContent = `${a.join('')}${operator}${b.join('')}`
     };
   });
 });
 
 
-function finalA(){
- let newA = a.splice(',');
- screenA = newA;
- console.log(newA);
-}
-console.log([a]);
-console.log([b]);
-
 
 sumBtn.addEventListener('click', ()=>{
-  operate(a, b);
+  let sum = operate(a, b);
+  screenSum.textContent = sum;
+  document.querySelector('.upperNum').classList.add('hideNull');
+  document.querySelector('.lowerNum').classList.add('displeySum');
+  console.log(operate());
 });
 
 
@@ -68,8 +99,8 @@ function divideNum(a, b) {
 
 
 function operate(){
-  let operandOne = a;
-  let operandTwo = b;
+  let operandOne = parseInt(a.join(''));
+  let operandTwo = parseInt(b.join(''));
   if(operator === '+'){
     let val = addNum(operandOne, operandTwo);
     return(val);
@@ -87,4 +118,3 @@ function operate(){
     return(val);
   };
 };
-console.log(operate());
